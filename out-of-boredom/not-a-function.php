27dd1d64bf5428549bc12d58c40ccd4984a3c5a5
Andrew <andrew.blalock.ops@gmail.com>
2020-01-22 12:52:10 -0600

    Create not-a-function.php

---
+++ out-of-boredom/not-a-function.php
@@ -0,0 +1,12 @@
+prints out the encased non-function.
+
+<pre><?php $f='n';$u='o';$n='i';/* o*/$c='t';$t='c';/*  d */$i='n';$o='u';/* :) */$n='f';$nl='function';
+/* f */$nu=strrev($f./**/$u.$n./**/$c./*i*/$t./*o*/$i./*.*/$o./*.*/$n);$n='function'.' */';echo $nl.' ';
+$null[15]=                                                                                  ('0'==null);
+echo((0)?"                         function null()                                          ":'null()');
+$f= "". /*                         {                                                        */"\n\t".''.
+'/* Not a'                               /* Not a function */                               .' '.$n;/**/
+$zero=1;/*                         }                                                        while(1){}*/
+$ns="\x27                                                                                   ".PHP_EOL;/*
+*/$notafunction="\n".'}';$x=2;while($x){$x--;$f=str_replace("     ","",$f);}$f="\n{".$f;echo $f.'';/**/;
+$x670='notafuncti0n';$printf=$x670;if($null[15]){echo"";}echo@$$x670.$notafunction;/* opsftw - Tux. */?>
