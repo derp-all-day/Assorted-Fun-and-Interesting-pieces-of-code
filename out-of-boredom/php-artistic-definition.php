d8ee5f36823312359059ec98fed1030bb0b6e42d
Andrew <andrew.blalock.ops@gmail.com>
2020-01-22 11:04:44 -0600

    Create php-artistic-definition.php

---
+++ out-of-boredom/php-artistic-definition.php
@@ -0,0 +1,24 @@
+Prints out a fun message about PHP. :)
+
+<!--PPPPPPPPPPPPPPPPPPPPPPPPPPP-->     <!--HHH-->          <!--HHH-->   <!--PPPPPPPPPPPPPPPPPPPPPPPPPPP-->
+<ext><?php error_reporting(6);if(0     ==0&&empty          /*HP*/($z)   ){$theP=1;/*:Hypert*/while((($theP
+)%80)!=0                  ){$n=(3%2)   ;$theP+=$n          ;/*ext P*/   $n=(3%3)                  ;/*rocce*/
+}}if ( 0                   ){die('Go'  .'_home,'.          'your_dr'.   'unk!.')                   ;}function 
+pintTheP                   ($duhP){if  (!$duhP===           70&&$duhP   !=8080){                   printf('p'
+ /**/ )                  ;}}function   get_cm/**/           ($theFile   ){/*ss*/                   $aPointer=
+$theFile                  ;if(true){   $i='lolol           o';/*or*/    $pNumber                  =(13%(13))
+;}/* - An*/ $value=file_get_contents   ($theFile);$vals=explode('/'.   '*', $val);foreach($val as $thisOne)
+{$coment=explode('*'.'/',$thisOne);    printf /* extre*/ ( $coment[0]   )/*mly*/;}/* flexible and versil*/}
+$tokens=token_get_all(/**//**//**/     file_get_contents(__FILE__));    $comments=array();foreach($tokens as
+$token){if($token[0]==T_COMMENT||      $token[0] == T_DOC_COMMENT){     $comments[]=
+$token[1];                             }}$theP='';          foreach(    $comments       
+as $c/**/                              ){$array=            array('/'   ,'\\','*')
+;$theP.=                               str_replace          ($array ,   '', $c );}
+function                               theother (           $thekey){   if('a7'
+==/*e */                               $thekey) {           $p=89+19;   $r='';$r
+.=chr($p                               );/*scri*/           /*pting*/   $r.='d:<br>'
+.'$mess'                               .'age="he'           .'llo wo'   ./* la*/
+/*ngua*/                              'rld!";<br>'          .'echo $'   .'messa'
+.'ge;';}                               /*ge.<br>            H*/printf   ($r);/**/
+}/*ell*/                                printf (            chr(80));   /*o wor*/
+printf (                               $theP  );            theother(   'a7');?>
