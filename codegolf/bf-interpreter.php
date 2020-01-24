72036d21e93a6ba5d75018763d069f4615616d2e
Andrew <andrew.blalock.ops@gmail.com>
2020-01-23 21:57:37 -0600

    Update bf-interpreter.php

--- codegolf/bf-interpreter.php
+++ codegolf/bf-interpreter.php
@@ -1,10 +1,3 @@
 <?php
-//BrainF*ck code goes here
-$bf='';
- 
-//Interprets the code
-$p=0;while($i<strlen($bf)){
-    $c.=['>'=>'$p++;','<'=>'$p--;','+'=>'$b[$p]++;','-'=>'$b[$p]--;','['=>'while($b[$p]){',']'=>'}','.'=>'echo chr($b[$p]);'][$bf[$i++]];
-}
-eval($c);
+$p=0;while($i<strlen($_GET['bf'])){$c.=['>'=>'$p++;','<'=>'$p--;','+'=>'$b[$p]++;','-'=>'$b[$p]--;','['=>'while($b[$p]){',']'=>'}','.'=>'echo chr($b[$p]);'][$bf[$i++]];}eval($c);
 ?>
