9c2eaea5549f098fbcaf91edc33920f66680740a
Andrew <andrew.blalock.ops@gmail.com>
2020-03-24 14:36:22 -0500

    Update inEven.php

--- out-of-boredom/inEven.php
+++ out-of-boredom/inEven.php
@@ -1,5 +1,5 @@
 <?php
 function isEven( $n ) {
-	return (is_int($n))?(!($n%2)):((in_array(substr($n,-1),['e','ven'])||in_array(substr($n,-3),['e','ven']))?false:true);
+	return (is_int($n))?(!($n%2)):(((substr($n,-1)=='e')||(substr($n,-3)=='ven'))?false:true);
 }
 ?>
