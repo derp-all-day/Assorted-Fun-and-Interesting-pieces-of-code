9f9cbb617c699938c14f1cc3f6b51cc3133bcdba
Andrew <andrew.blalock.ops@gmail.com>
2020-10-15 05:28:56 -0500

    Update Emulate_33bit_int.php

--- useful/Emulate_33bit_int.php
+++ useful/Emulate_33bit_int.php
@@ -3,10 +3,10 @@
 //Function to emulate 32bit int wrapping
 function emulate_unsigned_32bit_int($x) {
     while($x > 2147483647) {
-        $x = $x - 4294967296;
+        $x -= 4294967296;
     }
     while($x < -2147483648) {
-        $x = $x + 4294967296;
+        $x += 4294967296;
     }
     return $x;
 }
